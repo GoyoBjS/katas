bc2a5b4474e6afdc816b33d514c1e667e5007c0a
Gregorio Bujor Santos <57153783+GoyoBjS@users.noreply.github.com>
2023-04-23 23:37:23 +0200

    Add calculator without refactor

---
+++ calculator.ts
@@ -0,0 +1,28 @@
+export const calculator = (operations: string): number =>{
+  if(!operations) return 0;
+  const simbols: string[] = operations.split(" ");
+  let acc: number = 0;
+  for(let i = 0; i < simbols.length; i++){
+
+    const isNumber: number = Number(simbols[i]);
+    if(!isNaN(isNumber)){
+      acc += Number(simbols[i]);
+    } else if(simbols[i] === '-'){
+      simbols[i+1] = '-' + simbols[i+1];
+    }
+  }
+
+  return acc;
+}
+
+// export const calculator = (operations: string): number =>{
+//   if(!operations) return 0;
+//   const simbols: string[] = operations.split(" ");
+//   let acc: number = 0;
+//   simbols.forEach((symbol: string) =>{
+//     const isNumber: number = Number(symbol);
+//     if(!isNaN(isNumber)) acc += Number(symbol)
+//   })
+//   return acc;
+// }
+//
